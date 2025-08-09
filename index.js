065db0ffb87a48d32be4f0ca16d4a3b59a2ed1b2
BrianoFelps <briansticknodesbr@gmail.com>
2025-08-09 15:02:53 -0300

    Configuração do servidor, tipagem de importação ES, APIs adicionadas (modificações no package.json) e index.js criado

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 8080; 
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+})
