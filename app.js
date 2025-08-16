e48309f8f08c94f8deed16042c016ca53f3d8985
BrianoFelps <briansticknodesbr@gmail.com>
2025-08-16 08:46:00 -0300

    Criação de um arquivo de rotas, começo dos controllers

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import products from './routes/products'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use("/", routes);
+app.use("/", products);
 
 export default app;
---
+++ routes/products.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { getProducts } from '../controllers/productsController'
+
+export const router = express.Router();
+
+router.get()
