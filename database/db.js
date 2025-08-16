da98310429782ef85279bb9fbbd1b7bb087d889a
BrianoFelps <briansticknodesbr@gmail.com>
2025-08-16 07:38:16 -0300

    Finalização da conexão ao BD com variáveis conectadas ao dotenv

--- database/db.js
+++ database/db.js
@@ -4,10 +4,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'root',
-    password: '',
-    database: 'bdTreinamentoAPI'
-
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 })
+
+db.connect((err) => {
+    if(err) {
+        return console.error(`Erro ao se conectar ao banco de dados (${process.env.MYSQL_DATABASE}): ${err}`);
+    }
+    
+    if(process.env.MYSQL_HOST === 'localhost') console.log(`Usando servidor local`);
+    return console.log(`Conexão bem-sucedida ao banco de dados MySQL (${process.env.MYSQL_DATABASE})`);
+})
+
+export default db;
