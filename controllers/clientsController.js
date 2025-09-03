d54bebf50262b100e3f9e8dc6a96b947c9fe5326
BrianoFelps <briansticknodesbr@gmail.com>
2025-09-03 10:03:11 -0300

    Lógica de hash aplicada ao registro de usuário, na senha

--- controllers/clientsController.js
+++ controllers/clientsController.js
@@ -1,10 +1,14 @@
-import { regClient } from "../models/clientsModel.js"
+import { regClient } from "../models/clientsModel.js";
+import bcrypt from "bcrypt";
 
 export const registerClient = async(req, res) => {
     try{
         const user = req.body;
+        /*Criptografia de senha. Senha e numero de caracteres do salt*/
+        const hashedPassword = bcrypt.hashSync(user.password, 10);
+        user.password = hashedPassword;
         await regClient(user);
-        res.status(201).send()
+        res.status(201).send();
     } catch(err){   
         return res.status(500).send({message: err.message})
     }
